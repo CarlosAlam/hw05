dd50d71f10d62b086eea53db78ea0e13f8dee81c
CarlosAlam <alambataco@gmail.com>
2015-03-15 13:34:34 -0600

    Update hw_05_id_16.cpp

--- hw_05_id_16.cpp
+++ hw_05_id_16.cpp
@@ -1,19 +1,38 @@
-// Author:
-// Date:
-// Version:
+// Author:Martínez Castillo Carlos Alam
+// Date:22/02/15
+// Version:1.0
 
-/*
-  Homework DD
-  this assignment ...
-*/
 
+//Carlos Alam Martínez Castillo
+#include <stdio.h>
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 
-int main ()
+
+int main ( )
 {
-  // TODO your code here!!!!
+	char frase[50], res;
+	int x=0 , a=0;
+	
+	std::cout<<"introduce la frase a encriptar:";
+	gets (frase);
+	
+	while (frase[x] !='\0' && frase [x]>=0)//primero tenemos que decir que x es como nulo y despues que x es mas grande o igual a 0
+	{
+		x++;
+	}
+	std::cout<<"\n"<<x; 
+	for(x=x-1;x>=0;x--)
+	{
+		frase [x]=frase[x]-27+230;
+		std::cout<<" "<<frase[x];
+	}
+	
 
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+	
+	getch( );
+	return 0;
 }
+	
+	
